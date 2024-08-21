33e5df4a9afb90ab46473bcf69a7bc21cc5e72a3
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-08-21 07:40:03 +0600

    Header section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-      require('daisyui'),
-    ],
-  }
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ["./**/*.{html,js}"],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [
+//       require('daisyui'),
+//     ],
+//   }
